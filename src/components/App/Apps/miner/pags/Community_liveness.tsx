f42f30f5e9229713002749b14324b6358dfc24bc
CoNET Project <info@CoNETTech.ca>
2024-01-15 13:25:37 -0800

    20240115

--- src/components/App/Apps/miner/pags/Community_liveness.tsx
+++ src/components/App/Apps/miner/pags/Community_liveness.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react"
 import { useIntl } from "react-intl"
 import {cntp_address} from '../../../../../API/index'
 import {Grid, CircularProgress, TextField, styled as materialStyled, TableContainer, Typography, SvgIcon,Fab, ButtonProps,
-Table, TableHead, TableRow, TableCell, TableBody, IconButton, Paper, Stack, Box, Slide, Button} from '@mui/material'
+Table, TableHead, TableRow, TableCell, TableBody, IconButton, Paper, Stack, Box, Slide, Button, LinearProgress} from '@mui/material'
 import {grey, lightGreen, blueGrey, green, red} from '@mui/material/colors'
 import {startLiveness, stopLiveness, isLivenessRunning} from '../../../../../API/index'
 import miner2 from '../../../../../assets/miner/FancyNyan.webp'
@@ -33,6 +33,12 @@ const ColorButton = materialStyled(Button)<ButtonProps>(({ theme }) => ({
     borderRadius: '2rem'
 }))
 
+interface Ratedata {
+	ipaddress: number
+	online: number
+	rate: number
+	status: number
+}
 
 const Community_liveness = (CNTP: string, setCNTP: (v:string) => void, setTodayCNTP: (v:string) => void) => {
 	const intl = useIntl()
@@ -42,6 +48,37 @@ const Community_liveness = (CNTP: string, setCNTP: (v:string) => void, setTodayC
 	const [showInstanceError, setShowInstanceError] = useState(false)
 	const [minting, setMinting] = useState(false)
 	const [showTimeOutError, setShowTimeOutError] = useState(false)
+	const [regError, setRegError] = useState(false)
+	const [rateProgress, setRateProgress] = useState(0)
+	const [rateProgressBuffer, setRateProgressBuffer] = useState(0)
+	const [rateProgressColor, setRateProgressColor] = useState<'success'|'warning'|'error'>('success')
+	const [onlineCount, setOnlineCount] = useState(0)
+	const [onlineRate, setOnlineRate] = useState('')
+
+	const formatDat = (rateData: Ratedata) => {
+		let process = 0
+		if (rateData.online < 1000) {
+			process = (rateData.online/1000)*100
+			process = process > 90 ? 90 :process
+			setRateProgressColor('success')
+			
+		} else if (rateData.online < 2000) {
+			process = (rateData.online/2000)*100
+			process = process > 90 ? 90 :process
+			setRateProgressColor('warning')
+
+		} else {
+			process = (rateData.online/10000)*100
+			process = process > 90 ? 90 :process
+			setRateProgressColor('error')
+		}
+		setOnlineCount(rateData.online)
+		setRateProgress(process)
+		setRateProgressBuffer(process+Math.random()*7)
+		const cntpRate = (rateData.rate/(rateData.online*24)).toFixed(6)
+		setOnlineRate(cntpRate)
+	}
+
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -62,21 +99,30 @@ const Community_liveness = (CNTP: string, setCNTP: (v:string) => void, setTodayC
 						setShowLoader (false)
 					}
 					if(data[0] === 'TIMEOUT') {
-						
 						return setShowTimeOutError(true)
 					}
-					if (data[0] === 'FAILURE') {
-						
+					if (data[0] === 'sameInstance') {
 						return setShowInstanceError(true)
+					}
+
+					if (data[0]==='SAMEIP') {
+						return setShowSameIPError(true)
+					}
+					if (data[0]==='FAILURE') {
+						return setRegError(true)
 					}
 					if (!minting) {
 						setMinting(true)
 					}
 					setCNTP(data[0])
 					setTodayCNTP(data[1])
-					console.log ('showLoader',data)
+					//	@ts-ignore
+					const rateData: Ratedata = data[2]
+					formatDat(rateData)
 					
 				})
+
+				
 			}
 
 		}
@@ -104,6 +150,9 @@ const Community_liveness = (CNTP: string, setCNTP: (v:string) => void, setTodayC
 					setMinting(true)
 					setCNTP(data[0])
 					setTodayCNTP(data[1])
+					//	@ts-ignore
+					const rateData: Ratedata = data[2]
+					formatDat(rateData)
 				}
 			})
 		}
@@ -115,10 +164,10 @@ const Community_liveness = (CNTP: string, setCNTP: (v:string) => void, setTodayC
 
 	const clearError = () => {
 		
-			setShowSameIPError(false)
-			setShowTimeOutError(false)
-			setShowInstanceError(false)
-		
+		setShowSameIPError(false)
+		setShowTimeOutError(false)
+		setShowInstanceError(false)
+		setRegError(false)
 	}
 
 	const clickStart = async () => {
@@ -131,7 +180,7 @@ const Community_liveness = (CNTP: string, setCNTP: (v:string) => void, setTodayC
 		<ItemStyle3>
 			<Stack justifyContent="center" alignItems="center" spacing={2}>
 			{
-				(!showTimeOutError && !showInstanceError && !showSameIPError) &&
+				(!showTimeOutError && !showInstanceError && !showSameIPError) && !regError && ! minting &&
 				<Stack spacing={2}>
 					<Typography variant="body1" sx={{textAlign:'center', color: grey[500]}}>
 						{intl.formatMessage({id: 'platform.miner.community.liveness.detail'})}
@@ -141,6 +190,12 @@ const Community_liveness = (CNTP: string, setCNTP: (v:string) => void, setTodayC
 
 			}
 			
+			{
+				regError &&
+				<Typography variant="body1" sx={{textAlign:'center', color: red[700], padding: '0 0 1rem 0'}}>
+					{intl.formatMessage({id: 'platform.miner.community.liveness.sameIPError'})}
+				</Typography>
+			}
 			{
 				showSameIPError &&
 				<Typography variant="body1" sx={{textAlign:'center', color: red[700], padding: '0 0 1rem 0'}}>
@@ -165,6 +220,7 @@ const Community_liveness = (CNTP: string, setCNTP: (v:string) => void, setTodayC
 					{intl.formatMessage({id: 'platform.miner.community.liveness.sameMinerError'})}
 				</Typography>
 			}
+
 			<Typography variant="h5" sx={{textAlign:'center', color: green[500], fontWeight: '800'}}>
 				{CNTP} CNTP
 			</Typography>
@@ -173,23 +229,46 @@ const Community_liveness = (CNTP: string, setCNTP: (v:string) => void, setTodayC
 				minting? <img src={miner2} style={{width: '5rem'}} />: <img src={minerPause} style={{ width: '5rem'}} />
 			}
 			{
-				(showTimeOutError || showInstanceError) &&
+				(showTimeOutError || showInstanceError || showSameIPError || regError) &&
 				<ColorButton variant="outlined" color="error" endIcon={<ErrorIcon color="error" />} onClick={clickStart} sx={{ borderRadius: '2rem'}}>
 					<Typography  sx={{ fontStyle: "inherit", }}>
-						{
-							intl.formatMessage({id: 'platform.button.error'})
-						}
+						{intl.formatMessage({id: 'platform.miner.community.liveness.onlineMiners'})}
 					</Typography>	
 				</ColorButton>
 			}
 			{
-				!showTimeOutError && !showInstanceError &&
+				!showTimeOutError && !showInstanceError && !showSameIPError && !regError &&
 				<Stack spacing={1}>
 					{
-						minting &&
-						<Typography variant="body2" sx={{textAlign:'center', color: red[700], padding: '0 0 1rem 0'}}>
-							{intl.formatMessage({id: 'platform.miner.community.liveness.chainDelayInfo'})}
-						</Typography>
+						rateProgress > 0 && minting  &&
+						<Stack spacing={1}>
+							<Stack spacing={1} direction="row"  sx={{ fontStyle: "inherit"}}>
+								<Typography variant="caption" sx={{ color: green[700] }}>
+									{intl.formatMessage({id: 'platform.miner.community.liveness.onlineMiners'})}:
+								</Typography>
+								<Typography variant="caption" sx={{ color: '#9ccc65' }}>
+									{onlineCount} 
+								</Typography>
+								<Typography variant="caption" sx={{ color: green[700] }}>
+									{intl.formatMessage({id: 'platform.miner.community.liveness.rewordRate'})}:
+								</Typography>
+								<Typography variant="caption" sx={{ color:'#9ccc65' }}>
+									{onlineRate}
+								</Typography>
+
+								<Typography variant="caption" sx={{ color:green[700] }}>
+									{intl.formatMessage({id: 'platform.miner.community.liveness.eachMin'})}
+								</Typography>
+							</Stack>
+							
+							<LinearProgress 
+								variant="buffer" 
+								value={rateProgress} 
+								valueBuffer={rateProgressBuffer}
+								color={rateProgressColor}
+							/>
+						</Stack>
+						
 					}
 					
 					<ButtonNext
@@ -204,14 +283,25 @@ const Community_liveness = (CNTP: string, setCNTP: (v:string) => void, setTodayC
 							{intl.formatMessage({id: minting? 'platform.miner.register.MinerAni.stop':'platform.miner.register.button'})}
 					
 						</Typography>
-						{ showLoader && <CircularProgress size={20} color="success" variant="indeterminate"  />}
+
+						{ 
+							showLoader && <CircularProgress size={20} 
+							color="success" 
+							variant="indeterminate"  />
+						}
 						
 					</ButtonNext>
+					
 				</Stack>
 
 				
 			}
-			
+			{
+				minting &&
+				<Typography variant="caption" sx={{ fontStyle: "inherit", color: '#9ccc65'}}>
+					{intl.formatMessage({id: 'platform.miner.community.liveness.chainDelayInfo'})}
+				</Typography>
+			}
 			</Stack>
 		</ItemStyle3>
 		
